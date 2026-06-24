c5d980bd0f64531d4513536cbd530d8c34042060
alesaz <becario.adsaz@externomf.es>
2026-06-24 13:33:37 +0200

    eop update

--- add_predictions.py
+++ add_predictions.py
@@ -15,7 +15,7 @@ import os
 import matplotlib.pyplot as plt
 import sqlite3
 
-day = 61208
+day = 61215
 
 #Path to app local repository
 direc2 = "C:/Users/becario.adsaz/Documents/EOP_Predictions/stream_app"
