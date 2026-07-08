9de711f8a2f33631cf4e8db9b486c42915060b1a
alesaz <becario.adsaz@externomf.es>
2026-07-08 13:50:47 +0200

    EOP update & SLAM Data

--- add_predictions.py
+++ add_predictions.py
@@ -15,7 +15,7 @@ import os
 import matplotlib.pyplot as plt
 import sqlite3
 
-day = 61222
+day = 61229
 
 #Path to app local repository
 direc2 = "C:/Users/becario.adsaz/Documents/EOP_Predictions/stream_app"
